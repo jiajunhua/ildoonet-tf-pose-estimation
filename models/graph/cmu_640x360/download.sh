df668c77f4e31879f63be95acfb461f4f46ca6cf
Leighton Turner <leightonturner@gmail.com>
2018-01-30 22:38:37 +0000

    fix download urls

--- models/graph/cmu_640x360/download.sh
+++ models/graph/cmu_640x360/download.sh
@@ -2,7 +2,7 @@
 
 echo "download model graph : cmu_640x360"
 
-wget http://www.mediafire.com/file/fdhqpmdrdzafoc4/graph_freeze.pb -O graph_freeze.pb
-wget http://www.mediafire.com/file/n6qnqz00g1pjf7d/graph_opt.pb -O graph_opt.pb
-wget http://www.mediafire.com/file/38hyjrwfdsyqsbq/graph_q.pb -O graph_q.pb
-wget http://www.mediafire.com/file/a2a0nc8i1oj5iam/graph.pb -O graph.pb
+wget http://download1662.mediafire.com/9w41h1qg57og/fdhqpmdrdzafoc4/graph_freeze.pb -O graph_freeze.pb
+wget http://download1650.mediafire.com/35hcd7ukp3fg/n6qnqz00g1pjf7d/graph_opt.pb -O graph_opt.pb
+wget http://download1193.mediafire.com/eaoeszlwevfg/38hyjrwfdsyqsbq/graph_q.pb -O graph_q.pb
+wget http://download1477.mediafire.com/5mujvsj810xg/a2a0nc8i1oj5iam/graph.pb -O graph.pb
--- models/graph/cmu_640x480/download.sh
+++ models/graph/cmu_640x480/download.sh
@@ -2,8 +2,7 @@
 
 echo "download model graph : cmu_640x480"
 
-wget http://www.mediafire.com/file/q7fbue8dqr3ytrb/graph_freeze.pb -O graph_freeze.pb
-wget http://www.mediafire.com/file/eolfk6t1t3yb191/graph_opt.pb -O graph_opt.pb
-wget http://www.mediafire.com/file/s6d01qvmlkfxgzr/graph_q.pb -O graph_q.pb
-wget http://www.mediafire.com/file/ae7hud583cx259z/graph.pb -O graph.pb
-
+wget http://download1515.mediafire.com/66p3hu7p6lfg/q7fbue8dqr3ytrb/graph_freeze.pb -O graph_freeze.pb
+wget http://download1640.mediafire.com/vqciqfcbz7qg/eolfk6t1t3yb191/graph_opt.pb -O graph_opt.pb
+wget http://download843.mediafire.com/zczmlmayrrng/s6d01qvmlkfxgzr/graph_q.pb -O graph_q.pb
+wget http://download938.mediafire.com/3mootio0u5ag/ae7hud583cx259z/graph.pb -O graph.pb
