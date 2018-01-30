5410f7a79c4643a9c81eaa03c915ff1d2fed0008
Ildoo Kim (Curtis Kim) <ildoo@ildoo.net>
2018-01-30 12:32:09 +0900

    Update download.sh

--- models/graph/cmu_640x360/download.sh
+++ models/graph/cmu_640x360/download.sh
@@ -2,7 +2,7 @@
 
 echo "download model graph : cmu_640x360"
 
-wget https://www.dropbox.com/s/i442ol5ig3tn0h8/graph_freeze.pb?dl=0 -O graph_freeze.pb
-wget https://www.dropbox.com/s/ppo3b44jdfoz94x/graph_opt.pb?dl=0 -O graph_opt.pb
-wget https://www.dropbox.com/s/fvqx4r6idix15mn/graph_q.pb?dl=0 -O graph_q.pb
-wget https://www.dropbox.com/s/u7g1e4i1fc377rg/graph.pb?dl=0 -O graph.pb
+wget http://www.mediafire.com/file/fdhqpmdrdzafoc4/graph_freeze.pb -O graph_freeze.pb
+wget http://www.mediafire.com/file/n6qnqz00g1pjf7d/graph_opt.pb -O graph_opt.pb
+wget http://www.mediafire.com/file/38hyjrwfdsyqsbq/graph_q.pb -O graph_q.pb
+wget http://www.mediafire.com/file/a2a0nc8i1oj5iam/graph.pb -O graph.pb
