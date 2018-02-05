17d55445ca3cfa1e7181719f964a90f547137f4a
Hossein Hashemi <hossein.hashemi.ir@ieee.org>
2018-02-05 05:00:39 +0100

    Create run_video.py
    real-time pose estimation on video files.

---
+++ src/run_video.py
@@ -0,0 +1,60 @@
+import argparse
+import logging
+import time
+
+import cv2
+import numpy as np
+
+from estimator import TfPoseEstimator
+from networks import get_graph_path, model_wh
+
+logger = logging.getLogger('TfPoseEstimator-Video')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+fps_time = 0
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='tf-pose-estimation Video')
+    parser.add_argument('--video', type=str, default='')
+    parser.add_argument('--zoom', type=float, default=1.0)
+    parser.add_argument('--model', type=str, default='mobilenet_thin_432x368', help='cmu_640x480 / cmu_640x360 / mobilenet_thin_432x368')
+    parser.add_argument('--show-process', type=bool, default=False,
+                        help='for debug purpose, if enabled, speed for inference is dropped.')
+    args = parser.parse_args()
+
+    logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
+    w, h = model_wh(args.model)
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    #logger.debug('cam read+')
+    #cam = cv2.VideoCapture(args.camera)
+    cap = cv2.VideoCapture(args.video)
+    #ret_val, image = cap.read()
+    #logger.info('cam image=%dx%d' % (image.shape[1], image.shape[0]))
+    if (cap.isOpened()== False):
+        print("Error opening video stream or file")
+    while(cap.isOpened()):
+        ret_val, image = cap.read()
+
+
+        humans = e.inference(image)
+        image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+
+        #logger.debug('show+')
+        cv2.putText(image,
+                    "FPS: %f" % (1.0 / (time.time() - fps_time)),
+                    (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (0, 255, 0), 2)
+        cv2.imshow('tf-pose-estimation result', image)
+        fps_time = time.time()
+        if cv2.waitKey(1) == 27:
+            break
+
+
+    cv2.destroyAllWindows()
+logger.debug('finished+')
