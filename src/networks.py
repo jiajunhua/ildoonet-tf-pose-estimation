8116a7a0ac4f77bed8a32a6acd5318c76fb23f85
ildoonet <ildoo@ildoo.net>
2018-02-08 14:38:31 +0900

    Fix a bug which cause loading graph file path invalid in ROS

--- src/networks.py
+++ src/networks.py
@@ -73,9 +73,11 @@ def get_graph_path(model_name):
         'mobilenet_thin': './models/graph/mobilenet_thin/graph_opt.pb'
     }
     graph_path = dyn_graph_path[model_name]
-    if not os.path.isfile(graph_path):
-        raise Exception('Graph file doesn\'t exist, path=%s' % graph_path)
-    return graph_path
+    for path in (graph_path, os.path.join(os.path.dirname(os.path.abspath(__file__)), graph_path), os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', graph_path)):
+        if not os.path.isfile(path):
+            continue
+        return path
+    raise Exception('Graph file doesn\'t exist, path=%s' % graph_path)
 
 
 def model_wh(resolution_str):
