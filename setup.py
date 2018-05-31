c00954ab09359a3b294ba8933b0440f96f2da703
Ildoo Kim <ildoo@ildoo.net>
2018-05-31 16:49:54 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ subprocess.check_output(["bash", "models/graph/cmu/download.sh"], cwd=cwd)
 POSE_DIR = os.path.realpath(os.path.dirname(__file__))
 
 REQUIRED_PACKAGES = [
+    'argparse>=1.1',
     'dill==0.2.7.1',
     'fire >= 0.1.3',
     'matplotlib >= 2.2.2',
